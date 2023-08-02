c244062c84296e2fe9aa2e35b3a49ef05f3a983a
Lucas Dupias <lucas@codeacademyberlin.com>
2023-08-02 12:22:12 +0200

    dist

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,15 +4,15 @@ import connect from "./utils/connect";
 
 dotenv.config({ path: __dirname + "/.env" });
 
-const port = process.env.PORT;
+const port = 3000;
 const app = createServer();
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 app.listen(port, async () => {
-  console.log(`App is running at http://localhost:${port}`);
+  console.log(`App is running at http://localhost:3000`);
 
-  await connect();
+  // await connect();
 });
 
 export default app;
